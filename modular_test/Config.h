67ae7a1ef885a813b2c11074a70f1d449d629242
Matthew Gates <matthewg42@gmail.com>
2016-09-22 22:11:26 +0100

    brightness adjustment in the preprocessor

--- modular_test/Config.h
+++ modular_test/Config.h
@@ -7,13 +7,21 @@
 #define NEOPIXEL_PIN            4
 
 // VoltMode parameters
-#define VOLT_MODE_COLOR         0xFF500A
+#define VOLT_MODE_BASE_COLOR    0xFF500A
+// in a range of 0-255:
+#define VOLT_MODE_BRIGHTNESS    16
 // in 100ths of a volt:
 #define VOLT_MODE_VMIN          1000
 // in 100ths of a volt:
 #define VOLT_MODE_VMAX          1600
-#define VOLT_MODE_SLOWDOWN      6
+#define VOLT_MODE_SLOWDOWN      20
+
+// These are calculated by the preprocessor, so don't use CPU or RAM...  :)
 #define VOLT_MODE_TIME          ((VOLT_MODE_VMAX - VOLT_MODE_VMIN) * VOLT_MODE_SLOWDOWN)
+#define VOLT_MODE_COLOR_R       (((VOLT_MODE_BASE_COLOR >> 16)*VOLT_MODE_BRIGHTNESS)/256)
+#define VOLT_MODE_COLOR_G       ((((VOLT_MODE_BASE_COLOR >> 8)%256)*VOLT_MODE_BRIGHTNESS)/256)
+#define VOLT_MODE_COLOR_B       (((VOLT_MODE_BASE_COLOR%256)*VOLT_MODE_BRIGHTNESS)/256)
+#define VOLT_MODE_COLOR         ((VOLT_MODE_COLOR_R<<16)+(VOLT_MODE_COLOR_G<<8)+(VOLT_MODE_COLOR_B))
 
 // General config
 #define PEDAL_VOLTAGE_PIN       A0
--- modular_test/VoltMode.cpp
+++ modular_test/VoltMode.cpp
@@ -11,7 +11,8 @@ VoltMode::VoltMode() :
 void VoltMode::start()
 {
 #ifdef DEBUG
-    Serial.println(F("VoltMode start"));
+    Serial.print(F("VoltMode start, color:"));
+    Serial.println(VOLT_MODE_COLOR, HEX);
 #endif
     DisplayMode::start();
     _lastPixel = 0;
@@ -30,18 +31,24 @@ void VoltMode::stop()
 bool VoltMode::update()
 {
     uint16_t vIn = highVoltageConversion(PEDAL_VOLTAGE_PIN);
+#ifdef DEBUG
     Serial.print(F("vIn="));
     Serial.print(vIn);
+#endif
     if (vIn < VOLT_MODE_VMIN) {
         vIn = VOLT_MODE_VMIN;
     } else if (vIn > VOLT_MODE_VMAX) {
         vIn = VOLT_MODE_VMAX;
     }
+#ifdef DEBUG
     Serial.print(F(", clipped="));
     Serial.print(vIn);
+#endif
     vIn -= VOLT_MODE_VMIN;
+#ifdef DEBUG
     Serial.print(F(", adjust="));
     Serial.println(vIn);
+#endif
 
     if (_timeLeft > vIn) {
         _timeLeft -= vIn;
