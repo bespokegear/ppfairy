7013522e4c3466107167b7bb1670079387e4fc36
Matthew Gates <matthewg42@gmail.com>
2016-09-29 16:54:59 +0100

    clean up test rig

--- pattern_test/LEDs.cpp
+++
@@ -1,5 +0,0 @@
-#include "LEDs.h"
-#include "Config.h"
-
-static Adafruit_NeoPixel LEDs(NUMBER_OF_PIXELS, NEOPIXEL_PIN, NEOPIXEL_TYPE);
-
--- pattern_test/LEDs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Adafruit_NeoPixel.h>
-
-extern Adafruit_NeoPixel LEDs;
-
